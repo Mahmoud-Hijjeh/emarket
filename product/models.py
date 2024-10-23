0fd32fa17a6ef892dfff2305e677e2fa567e89d1
Mahmoud-Hijjeh <mahmoud2001asoom@gmail.com>
2024-10-23 18:23:59 +0300

    Adding Serializer for Product model

--- product/models.py
+++ product/models.py
@@ -17,3 +17,6 @@ class Product(models.Model):
     stock = models.IntegerField(default = 0)
     CreateAt = models.DateTimeField(auto_now_add = True)
     user = models.ForeignKey(User, null = True, on_delete = models.SET_NULL) 
+
+    def __str__(self) -> str:
+        return self.name
--- product/views.py
+++ product/views.py
@@ -3,9 +3,11 @@ from rest_framework.decorators import api_view
 # Create your views here.
 from rest_framework.response import Response
 from .models import Product
+from .serializers import ProductSerializer
 
 @api_view(['GET'])
 def get_all_products(request):
     products = Product.objects.all()
+    serilizer = ProductSerializer(products, many = True)
     print(products)
-    return Response({"muhammad":"Essa"})
+    return Response({"products":serilizer.data})
